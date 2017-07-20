f12c2058e3511117bac73302c7baf6b1757c299a
indali <indali.bora@gmail.com>
2017-07-20 13:29:30 -0700

    Update dictionary-attack.js

--- dictionary-attack.js
+++ dictionary-attack.js
@@ -15,5 +15,10 @@ window.onload = init;
 /* ADD YOUR CODE BELOW */
 
 function checkPassword() {
-
+if ("btnSubmit" = wordsList) {
+  display = "Weak password!";
+} else {
+  display = "Strong password!";
+}
+  
 }
